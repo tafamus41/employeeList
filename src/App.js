de1b5991dbb4e4d8a560de6fd3de2db3964c8129
asimov <seyranmustafa4@gmail.com>
2024-08-29 16:02:07 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import List from './components/List';
 import data from "./helper/data"
 
+
 function App() {
+ const [number,setNumber]=useState(1);
+ const changeNext=()=>setNumber(number+5);
+ const changePrev=()=>setNumber(number-5);
  
   return (
     <main>
       <section className="container">
         <h3>Employee List</h3>
         <h5>
-          (Employees 1 to 5)
+          (Employees {number} to {number+4})
         </h5>
         <List personel={data} />
         <div className='btns'>
-          <button >Prev</button>
-          <button >Next</button>
+          <button onClick={changePrev} >Prev</button>
+          <button onClick={changeNext} >Next</button>
         </div>
       </section>
     </main>
